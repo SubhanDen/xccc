afd2e231ef5b48e0d14a2616332d9c74de18f29d
KD Botz <98203719+x-kunal@users.noreply.github.com>
2022-03-13 21:49:52 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,12 +64,11 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+𝖩ᴏɪɴ ғᴏʀ 𝖬ᴏʀᴇ 𝖴ᴘᴅᴀᴛᴇ𝗌.
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
-        sendMarkup(' Join the mirror group to use this bot. -> <b>[ @Kd_botz ]</b>', context.bot, update, reply_markup)
+        sendMarkup('𝖩ᴏɪɴ ғᴏʀ 𝖬ᴏʀᴇ 𝖴ᴘᴅᴀᴛᴇ𝗌.', context.bot, update, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update)
